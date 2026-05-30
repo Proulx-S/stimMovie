8a7415749c07ed9332bf94ac115c6a320bdc1718
Sebastien Proulx <proulxs@stanford.edu>
2026-05-29 20:43:43 -0700

    decrease detection dificulty

--- doIt.m
+++ doIt.m
@@ -17,9 +17,10 @@ p = scaledNoiseMovieWithRedFix();          % get defaults (also prints them)
 % ── overrides ─────────────────────────────────────────────────────────────
 p.output.folder = fullfile('/local/users/Proulx-S/stim', 'scaledNoiseMovieWithRedFix/stim');
 % p.output.folder = fullfile('~/RemoteServer/takoyakiLocal/stim', 'scaledNoiseMovieWithRedFix/stim');
-p.temporal.durationS = 120; % seconds
-p.output.quality     =  70; % JPEG quality for Motion JPEG AVI (0–100)
+p.temporal.durationS =  120; % seconds
+p.output.quality     =   70; % JPEG quality for Motion JPEG AVI (0–100)
+p.fixDot.contrast    = 0.75; % contrast of the red fixation dot (0–1)
 
-nRunON  = 50;
-nRunOFF = 50;
+nRunON  = 30;
+nRunOFF = 30;
 scaledNoiseMovieWithRedFix(p, nRunON, nRunOFF);
